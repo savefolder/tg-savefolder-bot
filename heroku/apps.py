6f6001986851b0e2847ce9c90d111ed1dd281b58
packedbread <packedbread@example.com>
2019-06-30 19:03:05 +0300

    Fix calls

--- heroku/apps.py
+++ heroku/apps.py
@@ -13,14 +13,14 @@ class HerokuConfig(AppConfig):
 
     def ready(self):
         LOGGER.info('[Startup] Deleting previous webhooks')
-        res = methods.delete_webhook()
+        res = methods.delete_webhook.post()
         if res['code'] != 200:
             LOGGER.warning(f'[Startup] Failed to delete webhooks:\n{res}')
         else:
             LOGGER.info('[Startup] Successfully deleted previous webhooks')
 
         LOGGER.info('[Startup] Setting up new webhook')
-        res = methods.set_webhook({'url': settings.SELF_URL})
+        res = methods.set_webhook.post({'url': settings.SELF_URL})
         if res['code'] != 200:
             LOGGER.error(f'[Startup] Failed to set up new webhook:\n{res}')
         else:
