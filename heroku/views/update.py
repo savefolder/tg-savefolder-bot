120036e959abf6d059c34e6ae2319c733fae00ff
packedbread <packedbread@example.com>
2019-07-02 00:18:43 +0300

    Fix object creation

--- heroku/views/update.py
+++ heroku/views/update.py
@@ -21,7 +21,7 @@ class UpdateView(APIView):
         if update.get('message') is not None:
             message = update['message']
             from_user = message['from']
-            user_object = User.objects.get_or_create(
+            user_object, _ = User.objects.get_or_create(
                 id=from_user['id'],
                 is_bot=from_user['is_bot'],
                 first_name=from_user['first_name'],
@@ -29,15 +29,15 @@ class UpdateView(APIView):
                 username=from_user.get('username'),
             )
             chat = message['chat']
-            chat_object = Chat.objects.get_or_create(id=chat['id'], chat_type=chat['type'])
+            chat_object, _ = Chat.objects.get_or_create(id=chat['id'], chat_type=chat['type'])
             text = message.get('text', '')
-            message_object = Message.objects.create(
+            message_object, _ = Message.objects.get_or_create(
                 message_id=message['message_id'],
                 from_user=user_object,
                 date=datetime.fromtimestamp(message['date']),
                 chat=chat_object,
                 text=text,
             )
-            Update.objects.create(update_id=update['update_id'], message=message_object)
+            Update.objects.get_or_create(update_id=update['update_id'], message=message_object)
 
         return Response(status=HTTP_200_OK)
