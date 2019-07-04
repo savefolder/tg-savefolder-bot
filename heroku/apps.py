45513995d57ca2bb406e8fdb6f282ef04dcb40be
packedbread <packedbread@example.com>
2019-07-05 02:35:04 +0300

    Update webhooks

--- heroku/apps.py
+++ heroku/apps.py
@@ -14,19 +14,22 @@ class HerokuConfig(AppConfig):
     def ready(self):
         LOGGER.info('[Startup] Checking for existing webhooks')
         res = methods.updates.get_webhook_info.post()
-        if res['url'] == settings.FULL_UPDATE_URL:
-            LOGGER.info(f'[Startup] Found existing webhook for valid url: {res["url"]}')
-            return
-        elif res['url'] == '':
-            LOGGER.info('[Startup] No webhook found')
-        else:
-            LOGGER.info(f'[Startup] Found existing webhook for invalid url: {res["url"]}')
-            LOGGER.info('[Startup] Deleting previous webhooks')
-            res = methods.updates.delete_webhook.post()
-            if res['ok']:
-                LOGGER.info('[Startup] Successfully deleted previous webhooks')
+        if res['ok']:
+            if res['result']['url'] == settings.FULL_UPDATE_URL:
+                LOGGER.info(f'[Startup] Found existing webhook for valid url: {res["result"]["url"]}')
+                return
+            elif res['result']['url'] == '':
+                LOGGER.info('[Startup] No webhook found')
             else:
-                LOGGER.warning(f'[Startup] Failed to delete webhooks:\n{res}')
+                LOGGER.info(f'[Startup] Found existing webhook for invalid url: {res["result"]["url"]}')
+                LOGGER.info('[Startup] Deleting previous webhooks')
+                res = methods.updates.delete_webhook.post()
+                if res['ok']:
+                    LOGGER.info('[Startup] Successfully deleted previous webhooks')
+                else:
+                    LOGGER.warning(f'[Startup] Failed to delete webhooks:\n{res}')
+        else:
+            LOGGER.error('[Startup] Error getting webhook info, will attempt to set up webhook')
 
         LOGGER.info('[Startup] Setting up new webhook')
         res = methods.updates.set_webhook.post({'url': settings.FULL_UPDATE_URL})
