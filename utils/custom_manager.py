728065230770642df7b0c98133a15658fdccab09
packedbread <packedbread@example.com>
2019-07-13 10:11:12 +0300

    Change CustomManager

--- utils/custom_manager.py
+++ utils/custom_manager.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class CustomManager(models.Manager):
     def get_or_none(self, *args, **kwargs):
         try:
             result = self.get(*args, **kwargs)
-        except (ObjectDoesNotExist, MultipleObjectsReturned):
+        except ObjectDoesNotExist:
             result = None
         return result
